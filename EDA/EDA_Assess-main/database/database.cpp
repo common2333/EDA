550cbe0758e4f0664f6573068f0d748e4c9effe0
common2333 <148614779+common2333@users.noreply.github.com>
2023-11-19 00:23:43 +0800

    Add files via upload

---
+++ EDA/EDA_Assess-main/database/database.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+struct point{
+    int a;
+    int x;
+    int y;
+} C1,C2,C3,C4;
+struct net {
+    struct point;
+    int
+};
+struct situation {
+    struct piont C1,C2,C3,C4;
+    int  d;
+};
+struct  result{
+    int d;
+    struct point C1,C2,C3,C4;
+
+};
