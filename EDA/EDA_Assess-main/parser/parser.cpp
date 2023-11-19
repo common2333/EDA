8520be0928ea723abd810a7933513b7c5f2610db
common2333 <148614779+common2333@users.noreply.github.com>
2023-11-19 08:04:50 +0800

    Delete EDA directory

--- EDA/EDA_Assess-main/parser/parser.cpp
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    FILE *fp = NULL;
-
-    fp = fopen("case0.txt",);
-    fprintf(fp, "This is testing for fprintf...\n");
-    fputs("This is testing for fputs...\n", fp);
-    fclose(fp);
-}
